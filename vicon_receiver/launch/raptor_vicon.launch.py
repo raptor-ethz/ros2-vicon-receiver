2c6a68134ede78c78fb208a4ba9d12a7c4cca650
Pascal Strauch <pstrauch@ethz.ch>
2024-05-15 13:50:34 +0200

    Add static transforms

--- vicon_receiver/launch/raptor_vicon.launch.py
+++ vicon_receiver/launch/raptor_vicon.launch.py
@@ -14,6 +14,13 @@ def generate_launch_description():
             package='vicon_receiver', executable='vicon_client', output='screen',
             parameters=[{'hostname': hostname, 'buffer_size': buffer_size, 'namespace': topic_namespace}]
         ),
+
+        # tf2 format:
+        # x, y, z, qx, qy, qz, qw, parent, child
+        # with transformation child->parent (i.e. T_PC)
+
+        # Parent: ENU, Child: NED. 
+        # Transform NED->ENU: intrinsic XYZ 180° pitch, -90° yaw
         Node(package = "tf2_ros", 
             executable = "static_transform_publisher",
             arguments = ["0", "0", "0", "0.7071068", "0.7071068", "0", "0", "world", "world_ned"])
